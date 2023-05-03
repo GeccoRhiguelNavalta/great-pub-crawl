fa063dbf0491a901e5e588dc2744a7dfbbb7198b
GeccoRhiguelNavalta <gecno@hotmail.co.uk>
2023-05-03 14:59:46 +0100

    feat: initial next auth configured

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,28 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [],
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "hello@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        //handle auth!
+        const user = { id: "1", name: "Gecco", email: "test@test.com" };
+        return user;
+      },
+    }),
+  ],
 };
 
 const handler = NextAuth(authOptions);
