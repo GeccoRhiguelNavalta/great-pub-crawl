733e76538d5c143de6b21a7b31a05fc1f92c3cbe
GeccoRhiguelNavalta <gecno@hotmail.co.uk>
2023-05-16 11:15:46 +0100

    feat: add new pub page and form created

--- src/app/(app)/pub/add/page.tsx
+++ src/app/(app)/pub/add/page.tsx
@@ -1,9 +1,12 @@
 import Layout from "../../../../../components/ui/layout/layout";
+import { PubForm } from "./pubForm";
 
 function Pub() {
   return (
     <div className="h-screen w-screen flex justify-center items-center bg-slate-100">
-      <h1>Add pub page</h1>
+      <div className="flex flex-col justify-center items-center sm:shadow-xl pb-8 pt-12 sm:bg-white rounded-xl space-y-8">
+        <PubForm />
+      </div>
     </div>
   );
 }
---
+++ src/app/(app)/pub/add/pubForm.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import React, { useState } from "react";
+import { Label } from "../../../../../components/ui/label/label";
+import { Input } from "../../../../../components/ui/input/input";
+import { Alert } from "../../../../../components/ui/alert/alert";
+import { Button } from "../../../../../components/ui/button/button";
+
+export const PubForm = () => {
+  const [pubName, setPubName] = useState("");
+  const [error, setError] = useState<string | null>(null);
+
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("added pub!");
+    try {
+      const res = await fetch(`/api/pubs`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: pubName,
+        }),
+      });
+      if (res.ok) {
+        setPubName("");
+      } else {
+        setError((await res.json()).error);
+      }
+    } catch (error: any) {
+      setError(error?.message);
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit} className="p-12 space-y-10 w-full sm:w-[400px]">
+      <div className="grid w-full items-center gap-1.5">
+        <Label htmlFor="pubName">Pub Name</Label>
+        <Input
+          value={pubName}
+          onChange={(e) => setPubName(e.target.value)}
+          id="name"
+          type="name"
+        />
+      </div>
+      {error && <Alert>{error}</Alert>}
+      <div className="w-full">
+        <Button className="w-full" size="sm" onClick={onSubmit}>
+          Add Pub
+        </Button>
+      </div>
+    </form>
+  );
+};
