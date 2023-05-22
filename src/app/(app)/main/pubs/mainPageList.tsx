a8d7bc7605398e35c4ef3fdfb8d3b47d36af6c4c
GeccoRhiguelNavalta <gecno@hotmail.co.uk>
2023-05-22 16:43:41 +0100

    fix: main page UI/UX design fix

--- src/app/(app)/main/pubs/mainPageList.tsx
+++ src/app/(app)/main/pubs/mainPageList.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/app/components/ui/button/button";
 import { useEffect, useState } from "react";
 
 type PubProps = {
@@ -32,6 +33,8 @@ type Users = {
 
 export default function MainPageList() {
   const [allPubs, setAllPubs] = useState<PubProps>();
+  const [reviewClicked, setReviewClick] = useState(false);
+  const [visitorsClicked, setVisitorsClick] = useState(false);
 
   async function getAllPubs() {
     const pubs = await fetch("/api/pubs", {
@@ -68,26 +71,73 @@ export default function MainPageList() {
     };
   }
 
-  return (
-    <div className="h-full w-screen flex overflow-y-scroll flex-col justify-start items-center space-y-8 bg-slate-100">
-      {allPubs &&
-        allPubs.map((pub) => {
-          const { averageFoodRating, averageDrinkRating, averageRating } =
-            calculateAverages(pub.reviews);
-          return (
-            <div
-              key={pub.id}
-              className="w-[300px] h-full p-4 bg-white shadow-lg rounded grid grid-rows-7 space-y-3"
-            >
-              <div>{pub.name}</div>
-              <div>Reviews: {pub.reviews.length}</div>
-              <div>Visitors: {pub.visitors.length}</div>
-              <div>Average Food Rating: {averageFoodRating}</div>
-              <div>Average Drink Rating: {averageDrinkRating}</div>
-              <div>Average Rating: {averageRating}</div>
-            </div>
-          );
-        })}
-    </div>
-  );
+  if (reviewClicked && !visitorsClicked) {
+    return (
+      <>
+        <h1>reviews here</h1>
+        <button
+          onClick={() => setReviewClick(false)}
+          className="text-red-500 hover:text-red-700 hover:underline"
+        >
+          Back
+        </button>
+      </>
+    );
+  } else if (!reviewClicked && visitorsClicked) {
+    return (
+      <>
+        <h1>visitors here</h1>
+        <button
+          onClick={() => setVisitorsClick(false)}
+          className="text-red-500 hover:text-red-700 hover:underline"
+        >
+          Back
+        </button>
+      </>
+    );
+  } else {
+    return (
+      <div className="h-full w-screen flex overflow-y-scroll flex-col justify-start items-center space-y-8 bg-slate-100">
+        {allPubs &&
+          allPubs.map((pub) => {
+            const { averageFoodRating, averageDrinkRating, averageRating } =
+              calculateAverages(pub.reviews);
+            return (
+              <div
+                key={pub.id}
+                className="w-[300px] h-full p-4 bg-white shadow-lg rounded grid grid-rows-6 space-y-3"
+              >
+                <div className="font-bold text-center">{pub.name}</div>
+                <div className="grid grid-cols-2">
+                  Average Food Rating:
+                  <span className="font-light">{averageFoodRating}</span>
+                </div>
+                <div className="grid grid-cols-2">
+                  Average Drink Rating:
+                  <span className="font-light">{averageDrinkRating}</span>
+                </div>
+                <div className="grid grid-cols-2">
+                  Average Overall Rating:
+                  <span className="font-light">{averageRating}</span>
+                </div>
+                <Button
+                  onClick={() => setReviewClick(true)}
+                  className="grid grid-cols-2"
+                >
+                  Reviews:
+                  <span className="font-light">{pub.reviews.length}</span>
+                </Button>
+                <Button
+                  onClick={() => setVisitorsClick(true)}
+                  className="grid grid-cols-2"
+                >
+                  Visitors:
+                  <span className="font-light">{pub.visitors.length}</span>
+                </Button>
+              </div>
+            );
+          })}
+      </div>
+    );
+  }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Layout from "./components/ui/layout/layout";
 
 function MainPage() {
   return (
-    <main>
+    <main className="h-[550px] w-screen flex flex-col p-3 justify-center overflow-y-scroll space-y-6 items-center bg-slate-100">
+      <h1 className="font-bold">Pub Ratings</h1>
       <MainPageList />
     </main>
   );
