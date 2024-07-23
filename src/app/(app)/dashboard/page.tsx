cc1e46d50de4bb74ae57f9c11a448b1b8835287e
GeccoRhiguelNavalta <gecno@hotmail.co.uk>
2024-07-23 14:03:34 +0100

    redeploy

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 function Dashboard() {
   return (
-    <div className="h-[580px] md:h-[600px] lg:h-[750px] w-full flex flex-col p-3 justify-center overflow-y-scroll space-y-6 items-center bg-slate-100">
+    <div className="h-[580px] md:h-[600px] lg:h-[750px] w-full flex flex-col p-3 justify-center overflow-y-scroll space-y-6 items-center bg-slate-200">
       <div className="flex flex-col justify-center items-center pb-8 pt-10 w-[150px] space-y-8">
         <div className="w-full">
           <Link href="/pub/add">
